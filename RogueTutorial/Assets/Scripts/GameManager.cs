3a343b000fe6cb79cb91eb8f907c175b81de0230
Dlunoe <dlunoe@yahoo.com>
2019-11-19 20:05:41 -0700

    Added wake up function to GameManager script (uses level to create board)

--- RogueTutorial/Assets/Scripts/GameManager.cs
+++ RogueTutorial/Assets/Scripts/GameManager.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public BoardManager boardScript;
+
+    private int level = 3;
+
+
+    void Awake ()
+    {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
     // Start is called before the first frame update
     void Start()
     {
