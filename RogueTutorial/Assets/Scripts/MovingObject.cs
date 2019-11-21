2535a05c11d3780bf73239fe57ba14d5a7ed0a67
Dlunoe <dlunoe@yahoo.com>
2019-11-20 21:42:51 -0700

    declared function to attempt move

--- RogueTutorial/Assets/Scripts/MovingObject.cs
+++ RogueTutorial/Assets/Scripts/MovingObject.cs
@@ -18,7 +18,7 @@ public abstract class MovingObject : MonoBehaviour
         inverseMoveTime = 1f / moveTime;
     }
 
-    protected bool Move (int xDir, int yDir, out RaycastHit2D hit)
+    protected bool Move(int xDir, int yDir, out RaycastHit2D hit)
     {
         Vector2 start = transform.position;
         Vector2 end = start + new Vector2(xDir, yDir);
@@ -27,7 +27,7 @@ public abstract class MovingObject : MonoBehaviour
         hit = Physics2D.Linecast(start, end, blockingLayer);
         boxCollider.enabled = true;
 
-        if(hit.transform == null)
+        if (hit.transform == null)
         {
             StartCoroutine(SmoothMovement(end));
             return true;
@@ -47,7 +47,22 @@ public abstract class MovingObject : MonoBehaviour
         }
     }
 
-    protected abstract void CantMove <T> (T component)
+    protected virtual void AttemptMove<T>(int xDir, int yDir)
+        where T : Component
+    {
+        RaycastHit2D hit;
+        bool canMove = Move(xDir, yDir, out hit);
+
+        if (hit.transform == null)
+            return;
+
+        T hitComponent = hit.transform.GetComponent<T>();
+
+        if (!canMove && hitComponent != null)
+            OnCantMove(hitComponent);
+    }
+
+    protected abstract void OnCantMove <T> (T component)
         where T : Component;
     
 
