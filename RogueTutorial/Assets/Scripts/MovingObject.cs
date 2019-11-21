9a4003e45d1a0b1147b975b1cd8f9f644ba5d4a9
Dlunoe <dlunoe@yahoo.com>
2019-11-20 21:13:25 -0700

    Created public variables, set up component variables

---
+++ RogueTutorial/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour
+{
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+    }
+    
+   
+}
